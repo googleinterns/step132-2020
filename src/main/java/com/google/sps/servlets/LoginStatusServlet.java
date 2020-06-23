2b0681986f3c8ffb78179af0c37420c380c1b225
Sam Falberg <samuelfalberg@gmail.com>
2020-06-23 22:57:14 +0000

    Account for users who are already registered but not logged in

--- src/main/java/com/google/sps/servlets/LoginStatusServlet.java
+++ src/main/java/com/google/sps/servlets/LoginStatusServlet.java
@@ -20,6 +20,7 @@ import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -35,14 +36,52 @@ public class LoginStatusServlet extends HttpServlet {
 
         LoginStatus loginStatus;
 
-        //If user not logged in, show login form
+        // If user not logged in, show login form
         if (!userService.isUserLoggedIn()) {
-            loginStatus = new LoginStatus(false, userService.createLoginURL("/registration.html"));
-        } else {   //User is logged in, request can proceed
-            loginStatus = new LoginStatus(true, userService.createLogoutURL("/registration.html"));
+            loginStatus = new LoginStatus(false, false, userService.createLoginURL("/registration.html"));
+            String json = new Gson().toJson(loginStatus);
+            response.getWriter().println(json);
+            return;
+        } 
+        
+        String name = getName(userService.getCurrentUser().getUserId());
+        // Name is null if user hasn't registered, set needsToRegister to 'true' and make logout URL
+        if (name == null) {
+            loginStatus = new LoginStatus(true, true, userService.createLogoutURL("/homepage.html"));
+        } else {  // User is logged in and registered, make logout URL
+            loginStatus = new LoginStatus(true, false, userService.createLogoutURL("/homepage.html"));
         }
 
         String json = new Gson().toJson(loginStatus);
         response.getWriter().println(json);
+    }
+
+    /** Returns the name of the user with id, or null if the user has not yet registered. */
+    private String getName(String userId) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Query query = new Query("User");
+        PreparedQuery results = datastore.prepare(query);
+
+        Iterator<Entity> iteration = results.asIterator();
+        Entity userEntity = null;
+        // Check to see if user is in datastore
+        while(iteration.hasNext()) {
+            Entity entity = iteration.next();
+            String id = (String) entity.getProperty("userId");
+
+            if (id.equals(userId)) {
+                userEntity = entity;
+                break;
+            }
+        }
+
+        // User not registered
+        if (userEntity == null) {
+            return null;
+        }
+
+        // User has already registered, return their name
+        String name = (String) userEntity.getProperty("name");
+        return name;
     }
 }
