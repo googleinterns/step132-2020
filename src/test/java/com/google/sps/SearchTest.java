cae9b58df175197c8a72383f07b5a278462e913a
kashisharora <kashisharora@google.com>
2020-06-24 23:45:58 +0000

    Create test for empty response for topic with no tutors

--- src/test/java/com/google/sps/SearchTest.java
+++ src/test/java/com/google/sps/SearchTest.java
@@ -88,6 +88,30 @@ public final class SearchTest {
 
     }
 
+    @Test
+    public void doGetReturnsCorrectEmptyResponseForBusiness() throws IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class); 
+
+        when(request.getParameter("topic")).thenReturn("business");
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        //create the hard coded data
+        servlet.init();
+        servlet.doGet(request, response);
+
+        //verify that getParameter was called
+        verify(request, atLeast(1)).getParameter("topic"); 
+        writer.flush(); // it may not have been flushed yet...
+        //there are no tutors for business, so it should return an empty string
+        String expected = "";
+        Assert.assertTrue(stringWriter.toString().contains(expected));
+
+    }
+
     /**
     * This method converts a list of tutors to a JSON string.
     * @return String, the list of tutors as a JSON string
