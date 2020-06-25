60cc8eb6ed0b259563d0eb5b7c02561992e9047a
kashisharora <kashisharora@google.com>
2020-06-25 23:27:50 +0000

    Create search result elements for each result and append to page

--- src/main/webapp/search.js
+++ src/main/webapp/search.js
@@ -20,7 +20,7 @@ function redirectToResults() {
 
 /** Helper function for redirectToResults, used for testing purposes. */
 function redirectToResultsHelper(document, window) {
-    var topic = document.getElementById("topic-search-box").value;
+    var topic = document.getElementById("search-box-homepage").value;
 
     var url = "search-results.html?topic=" + encodeURIComponent(topic);
     window.location.href = url;
@@ -43,9 +43,47 @@ async function getSearchResultsHelper(document, window) {
     }
 
     if(topic != null) {
-        await fetch("/search?topic="+topic).then(response => response.text()).then((results) => {
+        await fetch("/search?topic="+topic).then(response => response.json()).then((results) => {
             var resultContainer = document.getElementById("result-container");
-            resultContainer.innerHTML = results;
+            var searchResultsFor = document.createElement("h4");
+            searchResultsFor.innerText = "Search results for " + topic + ":";
+            searchResultsFor.id = "search-results-topic";
+
+            resultContainer.appendChild(searchResultsFor);
+
+            results.forEach(function(result) {
+                resultContainer.append(createSearchResult(result));
+            });
         });
     }
+}
+
+function createSearchResult(result) {
+   result = JSON.parse(result);
+
+    var container = document.createElement("div");
+    var name = document.createElement("h3");
+    var email = document.createElement("h6");
+    var skills = document.createElement("p");
+    var availabilityLink = document.createElement("a");
+
+    name.innerText = result.name;
+    email.innerText = result.email;
+    skills.innerText = "Skills: " + result.skills.join(" ");
+    availabilityLink.innerText = "Availability";
+
+    availabilityLink.href = "/availability.html?tutorID=" + result.email;
+
+    container.classList.add("search-result");
+    container.classList.add("list-group-item");
+
+    // list-group-item list-group-item-action flex-column align-items-start
+
+    container.appendChild(name);
+    container.appendChild(email);
+    container.appendChild(skills);
+    container.appendChild(availabilityLink);
+
+    return container;
+
 }
