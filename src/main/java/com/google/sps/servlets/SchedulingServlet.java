823902f7a441da91cb339b0a22ada03ed704ea6b
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-06-26 15:41:30 +0000

    Write dopost function for the scheduling servlet.

--- src/main/java/com/google/sps/servlets/SchedulingServlet.java
+++ src/main/java/com/google/sps/servlets/SchedulingServlet.java
@@ -14,14 +14,28 @@
 
 package com.google.sps.servlets;
 
+import com.google.sps.data.Tutor;
+import com.google.sps.data.TimeRange;
+import com.google.sps.data.TutorSession;
+import com.google.sps.data.SampleData;
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*; // Can be deleted after hardcoded database is removed
 
 @WebServlet("/scheduling")
 public class SchedulingServlet extends HttpServlet {
+    SampleData hardcoded;
+
+    public void init(ServletConfig servletconfig) throws ServletException { 
+        hardcoded = new SampleData();
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -31,7 +45,33 @@ public class SchedulingServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("plain/text");
-        response.getWriter().println("To be implemented");
+        String tutorID = request.getParameter("tutorID");
+        String start = request.getParameter("start");
+        String end = request.getParameter("end");
+        String studentEmail = request.getParameter("studentEmail");
+        String subtopics = request.getParameter("subtopics");
+        String questions = request.getParameter("questions");
+
+        TimeRange timeslot = TimeRange.fromStartToEnd(Integer.parseInt(start), Integer.parseInt(end));
+
+        TutorSession tutoringSession = new TutorSession(studentEmail, tutorID, subtopics, questions, timeslot);
+
+        // Update scheduledSessions
+        hardcoded.addToScheduledSessionsByEmail(tutorID, tutoringSession);
+
+        // Remove available timeslot
+        hardcoded.deleteAvailabilityByTimeRange(tutorID, timeslot);
+
+        String json = convertToJsonUsingGson(hardcoded.getSampleTutors());
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+        return;
+    }
+
+    // Converts the hardcoded into a JSON string using the Gson library.
+    private String convertToJsonUsingGson(List<Tutor> tutors) {
+        Gson gson = new Gson();
+        String json = gson.toJson(tutors);
+        return json;
     }
 }
