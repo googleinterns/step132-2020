404ecd87d16d49deda83bccfd09865745eb6093c
kashisharora <kashisharora@google.com>
2020-06-26 17:38:07 +0000

    Create methods to search through hard coded data and return json

--- src/main/java/com/google/sps/servlets/SearchServlet.java
+++ src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -12,14 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.sps.servlets;
+package com.google.sps;
 
+import com.google.sps.data.SampleData;
+import com.google.sps.data.Tutor;
+import com.google.sps.data.TimeRange;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.lang.String;
+import com.google.gson.Gson;
 
 
 /** Servlet that returns search results (tutors and books) for a topic. */
@@ -29,14 +35,50 @@ public class SearchServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        response.setContentType("plain/text");
-        response.getWriter().println("Hello world");
+        String topic = request.getParameter("topic");
+
+        ArrayList<Tutor> results = getTutorsForTopic(topic);
+
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().println(convertListToJson(results));
        
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    /**
+    * Gets a list of tutors that have the specified topic as a skill.
+    * @return ArrayList<Tutor>
+    */
+    private ArrayList<Tutor> getTutorsForTopic(String topic) {
+        ArrayList<Tutor> results = new ArrayList<Tutor>();
 
+        for(Tutor tutor : SampleData.getSampleTutors()) {
+            String[] skills = tutor.getSkills();
+
+            for(String skill : skills) {
+                if(skill.toLowerCase().equals(topic.toLowerCase())) {
+                    results.add(tutor);
+                    break;
+                }
+            }
+        }
+
+        return results;
+    }
+
+    /**
+    * This method converts a list of tutors to a JSON string.
+    * @return String, the list of tutors as a JSON string
+    */
+    private String convertListToJson(ArrayList<Tutor> tutors) {
+        ArrayList<String> jsonTutors = new ArrayList<String>();
+        Gson gson = new Gson();
+        //convert all Tutor objects to JSON
+        for(Tutor t : tutors) {
+            jsonTutors.add(gson.toJson(t));
+        }
+
+        //convert list to JSON
+        return gson.toJson(jsonTutors);
     }
 
 }
