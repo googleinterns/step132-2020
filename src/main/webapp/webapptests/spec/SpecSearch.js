6d69c5345e54d3934eff2da003cd791177ab9f02
Kashish Arora <35456077+kashiish@users.noreply.github.com>
2020-06-30 10:25:32 -0400

    Add search functionality (#29)
    * Create methods to search through hard coded data and return json
    
    * Add bootstrap
    
    * Add boostrap classes for styling
    
    * Add bootstrap classes to search results page
    
    * Add JUnit test for doGet response
    
    * Add JUnit test for doGet empty response
    
    * Create test for empty search result
    
    * Check for empty string or null value for search topic
    
    * Creates search result element for each result
    
    * Create element to show number of results
    
    * Add styling for search form and results
    
    * Add tests for creating a search result element and loading results
    
    * Use simpler Gson conversion
    
    * Update JUnit test with Gson conversion

--- src/main/webapp/webapptests/spec/SpecSearch.js
+++ src/main/webapp/webapptests/spec/SpecSearch.js
@@ -17,14 +17,13 @@ describe("Search", function() {
     describe("when homepage needs to get redirected to search results", function() {
         var testSearchBox = document.createElement("input");
         testSearchBox.setAttribute("type", "text");
-        testSearchBox.id = "topic-search-box";
+        testSearchBox.className = "search-box";
         testSearchBox.setAttribute("value", "math") ;
 
         var mockWindow = {location: {href: "homepage.html"}};
 
         it("should change window.location.href to the correct search results url", function() {
-            // document.getElementById = jasmine.createSpy("search result").and.returnValue(testSearchBox);
-            spyOn(document, "getElementById").withArgs("topic-search-box").and.returnValue(testSearchBox);
+            spyOn(document, "getElementsByClassName").withArgs("search-box").and.returnValue([testSearchBox]);
             redirectToResultsHelper(document, mockWindow);
             expect(mockWindow.location.href).toEqual("search-results.html?topic=math");
         });
@@ -32,23 +31,59 @@ describe("Search", function() {
     });
 
     describe("when search results page is loaded", function() {
+        var testResults = ['{"name": "Tutor 1", "email": "tutor1@gmail.com", "skills": ["Math", "History"]}', 
+                            '{"name": "Tutor 2", "email": "tutor2@gmail.com", "skills": ["Math", "History"]}'];
+
         var resultContainer = document.createElement("div");
         resultContainer.id = "result-container";
 
         var mockWindow = {location: {href: "search-results.html?topic=math", search: "?topic=math"}};
-        var mockDocument = {getElementById: (id) => resultContainer};
 
-        it("should set resultContainer's text to results returned by fetch call", async function() {
-            spyOn(window, "fetch").and.callFake(function(servletUrl) {
-                return Promise.resolve({text: () => Promise.resolve("hello world.")});
-            });
-            await getSearchResultsHelper(mockDocument, mockWindow);
+        it("should create result elements inside resultContainer", async function() {
+            spyOn(window, "fetch").withArgs("/search?topic=math").and.returnValue(Promise.resolve({json: () => Promise.resolve(testResults)}));
+
+            spyOn(document, "getElementById").withArgs("result-container").and.returnValue(resultContainer);
+
+            await getSearchResultsHelper(document, mockWindow);
             
             expect(window.fetch).toHaveBeenCalledWith("/search?topic=math");
-            expect(resultContainer.innerHTML).toContain("hello world.");
+            //one for the number of results label + 2 for the number of results in testResults
+            expect(resultContainer.childNodes.length).toEqual(3);
+            expect(resultContainer.childNodes[0].innerText).toContain("Found 2 tutors for math");
         });
+
 
     });
 
+    describe("when a search result is created", function() {
+        var result = '{"name": "Tutor 1", "email": "tutor1@gmail.com", "skills": ["Math", "History"]}';
+        var element = createSearchResult(result);
 
+        it("should create div for result element", function() {
+            expect(element.tagName).toEqual("DIV");
+        });
+
+        it("should create h3 element inside result element for name", function() {
+            expect(element.childNodes[0].tagName).toEqual("H3");
+            expect(element.childNodes[0].innerText).toContain("Tutor 1");
+        });
+
+        it("should create h6 element inside result element for email", function() {
+            expect(element.childNodes[1].tagName).toEqual("H6");
+            expect(element.childNodes[1].innerText).toContain("tutor1@gmail.com");
+        });
+
+        it("should create p element inside result element for skills", function() {
+            expect(element.childNodes[2].tagName).toEqual("P");
+            expect(element.childNodes[2].innerText).toContain("Skills: Math, History");
+        });
+
+        it("should create anchor element inside result element for availability link", function() {
+            expect(element.childNodes[3].tagName).toEqual("A");
+            expect(element.childNodes[3].innerText).toContain("Availability");
+            expect(element.childNodes[3].href).toContain("/availability.html?tutorID=tutor1@gmail.com");
+        });
+
+    });
+    
 });
--- src/test/java/com/google/sps/SearchTest.java
+++ src/test/java/com/google/sps/SearchTest.java
@@ -14,17 +14,118 @@
 
 package com.google.sps;
 
+import com.google.sps.data.Tutor;
+import com.google.sps.data.TimeRange;
+import com.google.sps.data.TutorSession;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import com.google.gson.Gson;
+import java.io.*;
+import javax.servlet.http.*;
 
 @RunWith(JUnit4.class)
 public final class SearchTest {
 
+    private SearchServlet servlet;
+
+    @Before
+    public void setUp() {
+        servlet = new SearchServlet();
+    }
+
     @Test
-    public void noFailTest() {
-        Assert.assertEquals(true, true);
+    public void doGetReturnsCorrectResponseForHistory() throws IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class); 
+
+        when(request.getParameter("topic")).thenReturn("history");
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        //create the hard coded data
+        servlet.doGet(request, response);
+
+        //verify that getParameter was called
+        verify(request, atLeast(1)).getParameter("topic"); 
+        writer.flush(); // it may not have been flushed yet...
+        List<Tutor> expectedTutorList = Arrays.asList(new Tutor("Kashish Arora", "kashisharora@google.com", new String[]{"Math", "History"}, new TimeRange[]{TimeRange.fromStartToEnd(720, 780), TimeRange.fromStartToEnd(900,1020)}, new TutorSession[]{}));
+        String expected = new Gson().toJson(expectedTutorList);
+        Assert.assertTrue(stringWriter.toString().contains(expected));
+
+    }
+
+    @Test
+    public void doGetReturnsCorrectEmptyResponseForBusiness() throws IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class); 
+
+        when(request.getParameter("topic")).thenReturn("business");
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        //create the hard coded data
+        servlet.doGet(request, response);
+
+        //verify that getParameter was called
+        verify(request, atLeast(1)).getParameter("topic"); 
+        writer.flush(); // it may not have been flushed yet...
+
+        //there are no tutors for business, so it should return an empty string
+        String expected = "[]";
+        Assert.assertTrue(stringWriter.toString().contains(expected));
+
+    }
+
+    @Test
+    public void doGetReturnsCorrectErrorResponseForEmptyString() throws IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);       
+        HttpServletResponse response = mock(HttpServletResponse.class); 
+
+        when(request.getParameter("topic")).thenReturn("");
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        //create the hard coded data
+        servlet.doGet(request, response);
+
+        //verify that getParameter was called
+        verify(request, atLeast(1)).getParameter("topic"); 
+        writer.flush(); // it may not have been flushed yet...
+
+        //there are no tutors for business, so it should return an empty string
+        String expected = "{\"error\": \"Invalid search request.\"}";
+        Assert.assertTrue(stringWriter.toString().contains(expected));
+
+    }
+
+
+    /**
+    * This method converts a list of tutors to a JSON string.
+    * @return String, the list of tutors as a JSON string
+    */
+    private String convertListToJson(List<Tutor> tutors) {
+        ArrayList<String> jsonTutors = new ArrayList<String>();
+        Gson gson = new Gson();
+        //convert all Tutor objects to JSON
+        for(Tutor t : tutors) {
+            jsonTutors.add(gson.toJson(t));
+        }
+
+        //convert list to JSON
+        return gson.toJson(jsonTutors);
     }
 
 }
