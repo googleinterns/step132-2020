2d576001b20aaa2c4e217c5734d63b1f26a21c0e
kashisharora <kashisharora@google.com>
2020-07-07 15:38:53 +0000

    Create mock datastore service for testing search functionality

---
+++ src/main/java/com/google/sps/utilities/MockSearchDatastore.java
@@ -0,0 +1,52 @@
+
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.utilities;
+
+import com.google.sps.data.SampleData;
+import com.google.sps.data.Tutor;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.String;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/** Mock datastore service class used for search testing. */
+public final class MockSearchDatastore implements SearchDatastoreService {
+
+    /**
+    * Gets a list of tutors that have the specified topic as a skill.
+    * @return List<Tutor>
+    */
+    @Override
+    public List<Tutor> getTutorsForTopic(String topic, HttpServletResponse response) throws IOException {
+        ArrayList<Tutor> results = new ArrayList<Tutor>();
+
+        for(Tutor tutor : SampleData.getSampleTutors()) {
+            List<String> skills = tutor.getSkills();
+
+            for(String skill : skills) {
+                if(skill.toLowerCase().equals(topic.toLowerCase())) {
+                    results.add(tutor);
+                    break;
+                }
+            }
+        }
+
+        return results;
+    }
+}
