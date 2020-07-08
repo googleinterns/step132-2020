fd5b17b0e171ea9df59f2148e4c12c358fd031e0
kashisharora <kashisharora@google.com>
2020-07-08 18:40:45 +0000

    Implement addAvailability and deleteAvailability for real datastore

--- src/main/java/com/google/sps/utilities/RealAvailabilityDatastore.java
+++ src/main/java/com/google/sps/utilities/RealAvailabilityDatastore.java
@@ -28,6 +28,8 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
 import com.google.appengine.api.datastore.Transaction;
 import com.google.appengine.api.datastore.TransactionOptions;
 import java.util.Calendar;
@@ -49,6 +51,34 @@ public final class RealAvailabilityDatastore implements AvailabilityDatastoreSer
         List<TimeRange> timeslots = new ArrayList<TimeRange>();
 
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        Filter filter = new FilterPredicate("email", FilterOperator.EQUAL, email.toLowerCase());
+        Query tutorQuery = new Query("Tutor").setFilter(filter);
+
+        try {
+            PreparedQuery pq = datastore.prepare(tutorQuery);
+
+            //there should only be one result
+            Entity tutorEntity = pq.asSingleEntity();
+
+            timeslots = getTimeRanges(datastore, tutorEntity.getKey());
+
+        } catch(NullPointerException e) {
+            //the entity does not exist, skip to finally statement and return empty list
+        } 
+
+        return timeslots;
+    }
+
+    /**
+    * Adds a new time range to a tutor's availability.
+    * @return boolean, true if time was added, false if there was a problem adding it
+    */
+    @Override
+    public boolean addAvailability(String email, TimeRange time) {
+        boolean added = true;
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         TransactionOptions options = TransactionOptions.Builder.withXG(true);
         Transaction txn = datastore.beginTransaction();
 
@@ -61,31 +91,77 @@ public final class RealAvailabilityDatastore implements AvailabilityDatastoreSer
             //there should only be one result
             Entity tutorEntity = pq.asSingleEntity();
 
-            timeslots = getTimeRanges(datastore, txn, tutorEntity.getKey());
+            Entity timeslot = new Entity("TimeRange", tutorEntity.getKey());
+
+            datastore.put(txn, timeslot);
 
             txn.commit();
 
         } catch(NullPointerException e) {
-            //the entity does not exist, skip to finally statement and return empty list
+            //the tutor entity does not exist, return false
+            added = false;
         } finally {
           if (txn.isActive()) {
             txn.rollback();
           }
         }
 
-        return timeslots;
+        return added;
+    }
+
+    /**
+    * Deletes a time range from a tutor's availability.
+    * @return boolean, true if time was deleted, false if there was a problem deleting it
+    */
+    @Override
+    public boolean deleteAvailability(String email, TimeRange time) {
+        boolean deleted = true;
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        TransactionOptions options = TransactionOptions.Builder.withXG(true);
+        Transaction txn = datastore.beginTransaction();
+
+        //filter by tutor's email and time range properties
+        CompositeFilter timeFilter = CompositeFilterOperator.and(FilterOperator.EQUAL.of("email", email),
+                                                                 FilterOperator.EQUAL.of("start", time.getStart()), 
+                                                                 FilterOperator.EQUAL.of("end", time.getEnd()), 
+                                                                 FilterOperator.EQUAL.of("date", new Gson().toJson(time.getDate())));
+
+        Query query = new Query("TimeRange").setFilter(timeFilter);
+
+        try {
+            PreparedQuery pq = datastore.prepare(query);
+
+            //there should only be one result
+            Entity timeEntity = pq.asSingleEntity();
+
+            //delete from datastore
+            datastore.delete(txn, timeEntity.getKey());
+
+            txn.commit();
+
+        } catch(NullPointerException e) {
+            //the entity does not exist, return false
+            deleted = false;
+        } finally {
+          if (txn.isActive()) {
+            txn.rollback();
+          }
+        }
+
+        return deleted;
     }
 
     /**
     * Gets all the time range entities corresponding that have the given tutorId.
     * @return ArrayList<TimeRange>
     */
-    private ArrayList<TimeRange> getTimeRanges(DatastoreService datastore, Transaction txn, Key tutorKey) {
+    private ArrayList<TimeRange> getTimeRanges(DatastoreService datastore, Key tutorKey) {
         ArrayList<TimeRange> availability = new ArrayList<TimeRange>();
         
         //Use ancestor query to get all time ranges that belong to the tutor
         Query query = new Query("TimeRange", tutorKey);
-        PreparedQuery timeRanges = datastore.prepare(txn, query);
+        PreparedQuery timeRanges = datastore.prepare(query);
 
         for(Entity time : timeRanges.asIterable()) {
             availability.add(createTimeRange(time));
