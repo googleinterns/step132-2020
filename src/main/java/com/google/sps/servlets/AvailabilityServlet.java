42d1f656915bc2b6d51adce789496ad9024dc776
Kashish Arora <35456077+kashiish@users.noreply.github.com>
2020-07-09 14:56:02 -0400

    Integrate Datastore to get tutor availability (#48)
    * Create interface for accessing datastore to get tutor availability
    
    * Add new line at EOF
    
    * Create mock availability datastore class
    
    * Use mock datastore for availability test
    
    * Implement a real datastore service to get tutor availability
    
    * Catch null pointer exception in case tutor does not exist in datastore
    
    * Add comments clarifying empty constructor and servlet
    
    * Add addAvailability and deleteAvailability methods to availability datastore service
    
    * Implement addAvailability and deleteAvailability methods in mock datastore
    
    * Use ancestor query to get availability
    
    * Change return type of addAvailability and deleteAvailability to boolean
    
    * Implement addAvailability and deleteAvailability for real datastore
    
    * Cast time range properties to int instead of long
    
    * Revert "Cast time range properties to int instead of long"
    
    This reverts commit f0039cbe8160d10c7b21ecc2d98840272e69f84a.
    
    * Store time range properties in time range entity
    
    * Fix comment in catch block
    
    * Update mock datastore comment
    
    * Update java doc comments in availability datastore classes
    
    * Enable cross-group transactions
    
    * Store TimeRange entities without ancestor
    
    * Query time ranges and tutor sessions by tutor email
    
    * Remove addAvailability method from Tutor class to prevent merge conflicts
    
    * Revert "Remove addAvailability method from Tutor class to prevent merge conflicts"
    
    This reverts commit 0a9f686bfbccb75675110d5d36c3be51024dfb7a.
    
    * Rerun CLA test
    
    * Rerun Google CLA test
    
    * Rerun Google CLA test

--- src/main/java/com/google/sps/servlets/AvailabilityServlet.java
+++ src/main/java/com/google/sps/servlets/AvailabilityServlet.java
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//  
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -28,9 +28,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.sps.utilities.RealAvailabilityDatastore;
+import com.google.sps.utilities.MockAvailabilityDatastore;
+import com.google.sps.utilities.AvailabilityDatastoreService;
 
+/** Servlet that manages availability of a tutor. */
 @WebServlet("/availability")
 public class AvailabilityServlet extends HttpServlet {
+
+    private AvailabilityDatastoreService datastore;
+
+    /**
+    * Because we created a constructor with a parameter (the testing one), the default empty constructor does not work anymore so we have to explicitly create it. 
+    * We need the default one for deployment because the servlet is created without parameters.
+    */
+    public AvailabilityServlet(){}
+
+    public AvailabilityServlet(boolean test) {
+        if(test) {
+            datastore = new MockAvailabilityDatastore();
+        }
+    }
+
+    public void init() {
+        datastore = new RealAvailabilityDatastore();
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -43,14 +65,7 @@ public class AvailabilityServlet extends HttpServlet {
         // Get the id of the tutor whose availability will be displayed.
         String tutorID = request.getParameter("tutorID");
 
-        List<TimeRange> timeslots = new ArrayList<TimeRange>();
-
-        for (Tutor tutor : SampleData.getSampleTutors()) {
-            if (tutorID.toLowerCase().equals(tutor.getEmail().toLowerCase())) {
-                timeslots = tutor.getAvailability();
-                break;
-            }
-        }
+        List<TimeRange> timeslots = datastore.getAvailabilityForTutor(tutorID);
 
         String json = new Gson().toJson(timeslots);
         response.setContentType("application/json;");
