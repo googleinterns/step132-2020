3c90bdcef0196f3b703aeaf466fd2e7e807efc6c
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-07-16 23:46:14 +0000

    Fix transition from scheduling to confirmation

--- src/main/webapp/scheduling.js
+++ src/main/webapp/scheduling.js
@@ -12,49 +12,53 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function scheduleTutorSession() {
-    scheduleTutorSessionHelper(window);
+/** A function that adds event listeners to a DOM objects. */
+function addEventListeners() {
+    document.getElementById("scheduling-form").addEventListener('submit', event => {
+        event.preventDefault();
+        scheduleTutorSession(window);
+    });
 }
 
-function scheduleTutorSessionHelper(window) {
+
+function scheduleTutorSession(window) {
     getUserId().then(function(studentID) {
-        var queryString = new Array();
-        window.onload = readComponents(queryString, window);
-        const tutorID = queryString["tutorID"];
-        const start = queryString["start"];
-        const end = queryString["end"];
-        const year = queryString["year"];
-        const month = queryString["month"];
-        const day = queryString["day"];
-
-        var subtopics = document.getElementById("topics").value;
-        var questions = document.getElementById("questions").value;
-
-        const params = new URLSearchParams();
-        params.append('tutorID', tutorID);
-        params.append('start', start);
-        params.append('end', end);
-        params.append('year', year);
-        params.append('month', month);
-        params.append('day', day);
-        params.append('studentID', studentID);
-        params.append('subtopics', subtopics);
-        params.append('questions', questions);
-
-        // Redirect user to confirmation
-        fetch('/scheduling', {method: 'POST', body: params}).then(response => response.json()).then((tutors) => {
-            if(tutors.error) {
-                var message = document.createElement("p");
-                message.innerText = tutors.error;
-                document.body.appendChild(message);
-                return;
-            }
-
-            redirectToConfirmation(studentID, window);
-        });
-
+        scheduleTutorSessionHelper(window, studentID);
     });
+}
 
+function scheduleTutorSessionHelper(window, studentID) {
+    var queryString = new Array();
+    window.onload = readComponents(queryString, window);
+    const tutorID = queryString["tutorID"];
+    const start = queryString["start"];
+    const end = queryString["end"];
+    const year = queryString["year"];
+    const month = queryString["month"];
+    const day = queryString["day"];
+
+    const params = new URLSearchParams();
+    params.append('tutorID', tutorID);
+    params.append('start', start);
+    params.append('end', end);
+    params.append('year', year);
+    params.append('month', month);
+    params.append('day', day);
+    params.append('studentID', studentID);
+    params.append('subtopics', document.getElementById("topics").value);
+    params.append('questions', document.getElementById("questions").value);
+
+    // Redirect user to confirmation
+    fetch('/scheduling', {method: 'POST', body: params}).then(response => response.json()).then((tutors) => {
+        if(tutors.error) {
+            var message = document.createElement("p");
+            message.innerText = tutors.error;
+            document.body.appendChild(message);
+            return;
+        }
+
+        redirectToConfirmation(studentID, window);
+    });    
 }
 
 // Referenced to https://www.aspsnippets.com/Articles/Redirect-to-another-Page-on-Button-Click-using-JavaScript.aspx#:~:text=Redirecting%
