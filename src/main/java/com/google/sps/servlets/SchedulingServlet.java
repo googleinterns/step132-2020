ecd59a2326ae1035fffad1a8f47e3ba2a8f2eb51
kashisharora <kashisharora@google.com>
2020-07-16 21:44:44 +0000

    Remove doGet unimplemented doGet method from scheduling servlet

--- src/main/java/com/google/sps/servlets/SchedulingServlet.java
+++ src/main/java/com/google/sps/servlets/SchedulingServlet.java
@@ -41,12 +41,6 @@ public class SchedulingServlet extends HttpServlet {
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("plain/text");
-        response.getWriter().println("To be implemented");
-    }
-
-    @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json;");
 
