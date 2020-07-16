a75c75c2fd0bbceae68328531380f907df92b9be
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2020-07-16 14:22:02 +0000

    Create GoalDatastoreService.java

---
+++ src/main/java/com/google/sps/utilities/GoalDatastoreService.java
@@ -0,0 +1,68 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.utilities;
+
+import com.google.sps.data.TutorSession;
+import com.google.sps.data.TimeRange;
+import com.google.sps.data.Goal;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Transaction;
+import com.google.appengine.api.datastore.TransactionOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import java.lang.String;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Calendar;
+import com.google.gson.Gson;
+
+/** Accesses Datastore to manage goals. */ 
+public final class GoalDatastoreService {
+
+    /**
+    * Adds a new goal.
+    */
+    public void addGoal(Goal goal) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        TransactionOptions options = TransactionOptions.Builder.withXG(true);
+        Transaction txn = datastore.beginTransaction(options);
+
+        try {
+            Entity goalEntity = new Entity("Goal");
+
+            goalEntity.setProperty("studentID", goal.getStudentID());
+            goalEntity.setProperty("goal", goal.getGoal());
+
+            datastore.put(txn, goalEntity);
+
+            txn.commit();
+        } finally {
+          if (txn.isActive()) {
+            txn.rollback();
+          }
+        }
+    }
+
+}
