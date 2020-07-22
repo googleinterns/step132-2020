6f3299cee894f673f1aa4d29c873b39769e44db0
Bernardo Eilert Trevisan <43152751+betrevisan@users.noreply.github.com>
2020-07-22 18:35:57 -0400

    Emails (#88)
    * Add pom.xml
    
    * Add sendConfirmationEmail
    
    * Add methods to get student and tutor entities
    
    * Add functions to send out confirmation emails after session has been scheduled
    
    * Send email to tutor and student when a session is cancelled
    
    * Send email to user after they register
    
    * Update java tests to check if the email was successfully sent
    
    * Delete print statement
    
    * Update methods for emailing
    
    * Rerun cla

--- src/main/java/com/google/sps/servlets/RegistrationServlet.java
+++ src/main/java/com/google/sps/servlets/RegistrationServlet.java
@@ -22,15 +22,24 @@ import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import com.google.sps.data.TimeRange;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
 /** Servlet that handles user's registration info */
 @WebServlet("/registration")
@@ -98,6 +107,10 @@ public class RegistrationServlet extends HttpServlet {
         createStudentEntityAndPutInDatastore(datastore, studentEntity, fullName, bio, pfp, email, topicsToStr, userId);
     }
 
+    boolean testRegistrationEmail = sendRegistrationEmail(fullName, email);
+    
+    response.setContentType("application/json;");
+    response.getWriter().println("testRegistrationEmail: " + testRegistrationEmail);
     // TODO: Redirect back to page user was at before registration rather than always redirect to homepage, Issue #41
     response.sendRedirect("/homepage.html");
   }
@@ -140,4 +153,39 @@ public class RegistrationServlet extends HttpServlet {
     entity.setProperty("userId", userId);
     ds.put(entity);
   }
+
+  /**
+  * Sends a welcome email to the user after they register
+  */
+  public boolean sendRegistrationEmail(String name, String email) {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        String to = email;
+        String subject = "Welcome to Sullivan";
+        String userName = name;
+        String message = "Dear " + userName + ",\n" +
+                        "Welcome to Sullivan! You have successfully registered with us. " +
+                        "We are very happy to have you here!\n" + 
+                        "The Sullivan Team";
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("contact@icecube-step-2020.appspotmail.com", "Sullivan"));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            msg.setSubject(subject);
+            msg.setText(message);
+            Transport.send(msg);
+            return true;
+        } catch (AddressException e) {
+            System.out.println("Failed to set email address.");
+            return false;
+        } catch (MessagingException e) {
+            System.out.println("Failed to send email.");
+            return false;
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Failed to encode email.");
+            return false;
+        }
+    }
 }
